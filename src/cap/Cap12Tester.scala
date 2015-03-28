fdf79bc9463830c3a0f8c17c98334805aaef5b4c
Oleg <Oleg@Oleg-Lap>
2015-03-28 11:18:41 +0300

    fix files processing

--- src/cap/Cap12Tester.scala
+++ src/cap/Cap12Tester.scala
@@ -9,26 +9,28 @@ import org.apache.http.impl.client.HttpClients
 
 object Cap12Tester {
 
+  val date1 = "##Date##"
+  val datePlus = "##Date@@##"
+  val seq = "##NUM##"
+  val dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
+  val dateMilis = java.lang.System.currentTimeMillis + 45000
+  val currentDirectory = new java.io.File(".").getCanonicalPath + "/bin/data/"
+  val counterFileName = currentDirectory + "seq.xml"
+  
   def main(args: Array[String]) {
-//    println("Working ....")
-    val date1 = "##Date##"
-    val datePlus = "##Date@@##"
-    val seq = "##NUM##"
-    val dateFormat = "yyyy-MM-dd'T'HH:mm:ss"
-    val dateMilis = java.lang.System.currentTimeMillis + 45000
-    def currentDirectory = new java.io.File( "." ).getCanonicalPath +"/bin/data/"
+    //    println("Working ....")
 
     val currDate = new java.text.SimpleDateFormat(dateFormat).format(new java.util.Date) + "+02:00"
     val currDatePlus = new java.text.SimpleDateFormat(dateFormat).format(new java.util.Date(dateMilis)) + "+02:00"
-    val seqNum = scala.io.Source.fromFile(currentDirectory +"seq.xml").mkString.toInt + 1
+    val seqNum = scala.io.Source.fromFile(counterFileName).mkString.toInt + 1
     val seqNumStr = seqNum.toString()
-    println( "currentDirectory:"+currentDirectory)
-    val xmlIn = scala.xml.XML.loadFile(currentDirectory+"soap12.xml").mkString
+    println("currentDirectory:" + currentDirectory)
+    val xmlIn = scala.xml.XML.loadFile(currentDirectory + "soap12.xml").mkString
     val soapXml = xmlIn.replaceAll("##Date##", currDate).replaceAll("##NUM##", seqNumStr).replaceAll(datePlus, currDatePlus)
-//    println(soapXml)
+    //    println(soapXml)
     sendSoap12Cap12(soapXml)
-    fileWriter( seqNumStr)
-//    println("___end___")
+    fileWriter(seqNumStr)
+    //    println("___end___")
   }
 
   def readLoadedMovies: Set[String] = {
@@ -53,8 +55,8 @@ object Cap12Tester {
    *
    */
   def fileWriter(seqNum: String) {
-    val fw = new FileWriter("d:/Downloads/  seq.xml", false)
-    println("seq:"+seqNum)
+    val fw = new FileWriter(counterFileName, false)
+    println("seq:" + seqNum)
     fw.write(seqNum)
     fw.close()
   }
@@ -71,7 +73,7 @@ object Cap12Tester {
     val httpClient = HttpClients.createDefault()
     val response = httpClient.execute(httpPost)
     val body = response.getEntity().getContent()
-    
+
     println("----------response------------")
     println(scala.io.Source.fromInputStream(body).mkString)
     println("status code: " + response.getStatusLine().getStatusCode()
